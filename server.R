e2f7819a5051f7fe985d2996938bd7a1cd66c5f3
kennethinfante <infantekennethjhim@gmail.com>
2024-12-01 15:27:16 +0800

    Added the Netsuite panel

--- server.R
+++ server.R
@@ -16,9 +16,20 @@ date_filters <- read_csv("data-raw/date_filters.csv")
 date_filters <- date_filters %>% arrange(desc(year), quarter_name, month)
 
 bs_updated <- 0
+ns_bs_updated <- 0
 
 function(input, output, session) {
   
+  ################## Helpers ##################
+  try_filter <- function(.data, col, selection) { 
+    if (!is.null(selection)) {
+      .data %>% filter({{col}} %in% isolate({{selection}}))
+    } else {
+      .data
+    }
+  }
+  
+  ################## Adjusted ##################
   updateCheckboxGroupInput(session,
                     "selected_year",
                     choices = unique(date_filters$year))
@@ -48,14 +59,6 @@ function(input, output, session) {
                                    choices = month_opt)
                  
                })
-    
-  try_filter <- function(.data, col, selection) { 
-    if (!is.null(selection)) {
-      .data %>% filter({{col}} %in% isolate({{selection}}))
-    } else {
-      .data
-    }
-  }
   
   output$balance_sheet <- renderPivottabler({
     
@@ -96,6 +99,78 @@ function(input, output, session) {
       paste("Last updated ", format(Sys.time(), "%b %d %Y %H:%M:%S"))
     }
 
+  })
+  
+################## Netsuite ##################
+  updateCheckboxGroupInput(session,
+                           "ns_selected_year",
+                           choices = unique(date_filters$year))
+  
+  observeEvent(input$ns_selected_year,
+               {
+                 quarter_opt <- date_filters %>%
+                   filter(year %in% input$ns_selected_year) %>%
+                   distinct(quarter_name) %>% 
+                   pull(quarter_name)
+                 
+                 updateCheckboxGroupInput(session,
+                                          "ns_selected_quarter",
+                                          choices = quarter_opt)
+                 
+               })
+  
+  observeEvent(input$ns_selected_quarter,
+               {
+                 month_opt <- date_filters %>%
+                   filter(year %in% input$ns_selected_year) %>%
+                   filter(quarter_name %in% input$ns_selected_quarter) %>%
+                   pull(month_name)
+                 
+                 updateCheckboxGroupInput(session,
+                                          "ns_selected_month",
+                                          choices = month_opt)
+                 
+               })
+  
+  output$ns_balance_sheet <- renderPivottabler({
+    
+    print(input$ns_update_balance_sheet)
+    
+    if(input$ns_update_balance_sheet == 0){
+      return()
+    }
+    
+    # created a custom filter function
+    bs_filtered <- bs_all %>%
+      try_filter(year, input$ns_selected_year) %>%
+      try_filter(quarter_name, input$ns_selected_quarter) %>%
+      try_filter(month_name, input$ns_selected_month)
+    
+    pt <- PivotTable$new(totalStyle = list(
+      "font"="1em arial",
+      "font-weight"="bold",
+      "background-color"="#F2F2F2"))
+    pt$addData(bs_filtered)
+    pt$addColumnDataGroups("year", addTotal=FALSE)
+    pt$addColumnDataGroups("quarter_name", addTotal=FALSE)
+    pt$addColumnDataGroups("month_name", addTotal=FALSE)
+    pt$addRowDataGroups("NS_BS_Flag", addTotal=FALSE)
+    pt$addRowDataGroups("ns_category")
+    pt$addRowDataGroups("account_full_name", addTotal=FALSE)
+    pt$defineCalculation(calculationName="Total", summariseExpression="sum(ns_std_amount_gbp)", format=list(nsmall=0, big.mark=",", decimal.mark=".", scientific=FALSE))
+    pt$evaluatePivot()
+    
+    pivottabler(pt)
+    
+  })
+  
+  output$ns_last_update <- renderText({
+    # another way of updating global variable
+    if (input$ns_update_balance_sheet > ns_bs_updated) {
+      ns_bs_updated <<- input$ns_update_balance_sheet
+      paste("Last updated ", format(Sys.time(), "%b %d %Y %H:%M:%S"))
+    }
+    
   })
   
 }
--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ library(shinycustomloader)
 navbarPage(
   "Balance Sheet",
   tabPanel(
-    "Adjusted",
+    "Per Adjusted",
     fluidPage(fluidRow(
       column(
         checkboxGroupInput("selected_year",
@@ -30,6 +30,33 @@ navbarPage(
     actionButton("update_balance_sheet", label = "Update Balance Sheet", width = "100%"),
     textOutput("last_update"),
     withLoader(pivottablerOutput('balance_sheet'))
+  ),
+  tabPanel(
+    "Per Netsuite",
+    fluidPage(fluidRow(
+      column(
+        checkboxGroupInput("ns_selected_year",
+                           "Select a year",
+                           choices = NULL),
+        width = 3
+      ),
+      column(
+        checkboxGroupInput("ns_selected_quarter",
+                           "Select a quarter",
+                           choices = NULL),
+        width = 3
+      ),
+      column(
+        checkboxGroupInput("ns_selected_month",
+                           "Select an month",
+                           choices = NULL,
+                           width = "100%"),
+        width = 3
+      )
+    )),
+    actionButton("ns_update_balance_sheet", label = "Update Balance Sheet", width = "100%"),
+    textOutput("ns_last_update"),
+    withLoader(pivottablerOutput('ns_balance_sheet'))
   ),
   collapsible = TRUE
 )
